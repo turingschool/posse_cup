498c88809f2471e5d0f1a19c0acfb7e6d4c973e2
worace <horace.d.williams@gmail.com>
2015-05-18 18:56:54 -0600

    skip csrf

--- app/controllers/api/v1/commands_controller.rb
+++ app/controllers/api/v1/commands_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CommandsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
   def create
     render Commands.parse(params[:text], params.slice(:token, :user_id)).response.symbolize_keys
   end
