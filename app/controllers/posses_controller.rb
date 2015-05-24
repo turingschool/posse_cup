1a9cacab70d9c7e82907ab32be20dcc892cc9467
worace <horace.d.williams@gmail.com>
2015-05-23 22:31:21 -0600

    expose posses on a page

---
+++ app/controllers/posses_controller.rb
@@ -0,0 +1,2 @@
+class PossesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "standings#index"
+  resources :posses, only: [:index]
   namespace :api do
     namespace :v1 do
       resources :commands, only: [:create]
