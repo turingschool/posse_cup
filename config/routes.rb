94ec553bcca7e38935b26ba9b49643cfed7df148
worace <horace.d.williams@gmail.com>
2015-09-09 16:57:40 -0600

    add basic oauth implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "standings#index"
   resources :posses, only: [:index]
+  get "/login", to: redirect("/auth/slack")
+  get "/auth/slack/callback" => "sessions#create"
   namespace :api do
     namespace :v1 do
       resources :commands, only: [:create]
