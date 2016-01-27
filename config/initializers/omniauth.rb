1991cfdb15f4bc204b36e6e00e081a26afaa8c8c
Horace Williams <horace.d.williams@gmail.com>
2016-01-27 11:39:20 -0700

    update slack scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :slack, ENV["SLACK_CLIENT_ID"], ENV["SLACK_CLIENT_SECRET"], scope: "client"
+  provider :slack, ENV["SLACK_CLIENT_ID"], ENV["SLACK_CLIENT_SECRET"], scope: "channels:history,channels:read,team:read,usergroups:read,users:read,identify"
 end
