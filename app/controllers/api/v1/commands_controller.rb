10b1ea57c9e924936d66da6db5f8fee0f7503020
Horace Williams <horace.d.williams@gmail.com>
2016-03-22 09:50:55 -0600

    RIP IN PIECES POSSE CUP

--- app/controllers/api/v1/commands_controller.rb
+++ app/controllers/api/v1/commands_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::CommandsController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
   def create
-    render Commands.parse(params[:text], params.slice(:token, :user_id)).response.symbolize_keys
+    render {json: {text: "RIP IN PIECES POSSE CUP" }, status: 200}
+    # render Commands.parse(params[:text], params.slice(:token, :user_id)).response.symbolize_keys
   end
 end
