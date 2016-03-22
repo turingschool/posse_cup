29e4c0ec1b568bb65c3e053e048755fc409d09ce
Horace Williams <horace.d.williams@gmail.com>
2016-03-22 09:53:05 -0600

    copy lol

--- app/controllers/api/v1/commands_controller.rb
+++ app/controllers/api/v1/commands_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CommandsController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token
   def create
-    render({json: {text: "RIP IN PIECES POSSE CUP" }, status: 200})
+    render({json: {text: "RIP IN PEACES POSSE CUP" }, status: 200})
     # render Commands.parse(params[:text], params.slice(:token, :user_id)).response.symbolize_keys
   end
 end
